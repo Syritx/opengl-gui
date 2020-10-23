dc8fde8d559712eb6c0e51a26e3fe7d87e3fef3f
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-10-23 16:07:48 -0400

    just a glfw test

--- main.cpp
+++ main.cpp
@@ -1,25 +1,34 @@
+#define GL_SILENCE_DEPRECATION
+#include <iostream>
+
+#define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(int argc, const char * argv[]) {
-
+    
     GLFWwindow* window;
     if (!glfwInit()) return -1;
     
-    window = glfwCreateWindow(1000, 720, "test opengl", NULL, NULL);
+    window = glfwCreateWindow(1100, 820, "Hello World", NULL, NULL);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
     
     if (!window) {
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
-    
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
+
     glfwTerminate();
     return 0;
 }
